894abcc20415af9f2b645103e105f437458c4f40
bakari <aboousmane@gmail.com>
2017-12-28 02:41:34 -0800

    experimentation du nouveau logo (new logo experimentation)

--- resources/views/layouts/vue.blade.php
+++ resources/views/layouts/vue.blade.php
@@ -6,13 +6,16 @@
 <link href="{{ asset('css/mystyle.css') }}" rel="stylesheet">
         </head>
         <body>
-                <nav class="navbar navbar-toggleable-md navbar-light bg-faded" id="mynav">
+                <nav style="height: 60px !important;padding: 40px 0px 30px 0px !important" class="navbar navbar-toggleable-md navbar-light bg-faded" id="mynav">
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                           <span class="navbar-toggler-icon"></span>
                         </button>
                         <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-                         <div id="mynav1"> <a class="navbar-brand"   href="#" id="mynav1">IslamApp</a></div>
-                          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                         <div id="mynav1"> 
+                          <a class="navbar-brand"   href="#" id="mynav1">
+                            <img style="width: 50%;height: 50%;text-shadow: 0px 5px #000" src="{{ asset('images/small_logo.png') }}" alt=""></a>
+                         </div>
+                          <ul style="margin-top: 60px !important" class="navbar-nav mr-sm-5 mt-2 mt-lg-0">
                             <li class="nav-item active">
                               <a class="nav-link"  href="#" id="mynav1">Conference <span class="sr-only">(current)</span></a>
                             </li>
@@ -32,12 +35,12 @@
                                   <a class="nav-link" href="#" id="mynav1">Questions/reponses</a>
                                 </li>
                               </ul>
-                              <form class="form-inline my-2 my-lg-0">
-                                <input class="form-control mr-sm-2" type="text" placeholder="recherche">
+                              <form class="form-inline my-lg-0">
+                                <input class="form-control" type="text" placeholder="recherche">
                                 <button class="btn btn-success my-2 my-sm-0" type="submit">recherche</button>
                               </form>
                             </div>
-                          </nav>
+                     </nav>
                            <div class="alert alert-success marouf">
                           
                             <h1 class="col-md-8">La plate forme d'apprentissage et connaissance de l'islam</h1>
