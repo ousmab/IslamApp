24b76566494eec64c81d77d96b9e4763fcb310e7
SALY ABBO <elmaroufa@gmail.com>
2020-11-14 12:54:33 +0100

    creation controlleur et vue pour la gestion des questions en attente de validation

--- application/plugins/questions/controllers/question_controller.py
+++ application/plugins/questions/controllers/question_controller.py
@@ -1,6 +1,13 @@
 from application.plugins.questions import app_question
 from application.plugins.questions.models.question_model import QuestionModel
+from flask import render_template, flash, request, redirect, url_for, abort
+from application.plugins.theme.models.theme_model import Theme
 
-@app_question.route('/questions/')
+'''
+controlleur pour afficher les questions en cour de validation
+'''
+@app_question.route('/admin/question/all_question')
 def all_questions():
-    return 'dddd'
+    theme_online = Theme.query.filter_by(is_brouillon=False, is_archive=False).first()
+    question_no_validate = QuestionModel.query.filter_by(is_approved=False, theme_id= theme_online.id)
+    return render_template('admin/all_question.html',questions=question_no_validate)
