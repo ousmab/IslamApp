0bfc185e41436df9c1b5ce74b95748e7b0dce557
SALY ABBO <elmaroufa@gmail.com>
2020-11-14 03:15:46 +0100

    travaux avant le merge le vrai de abdou nassser en ligne

---
+++ application/env.py
@@ -0,0 +1,4 @@
+# DATABASE CONFIG
+
+DATABASE_URI = 'postgresql+psycopg2://admin:adminpgdc@localhost:5432/islamappdb'
+# Exemple: DATABASE_URI = 'postgresql+psycopg2://admin:admin@172.20.0.2:5432/islamapp'
